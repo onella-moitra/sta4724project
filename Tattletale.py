d45d2ac2f4970d0f55e8ecb142c8ed5f147fb52c
wiillrochaa <willarocha03@gmail.com>
2025-04-11 16:35:44 -0400

    Update w/ Threshold Tattletale.py

--- Tattletale.py
+++ Tattletale.py
@@ -12,3 +12,11 @@ Anthony Beuke
 '''
 import cv2
 import numpy as np
+
+#Threshold Code (I'm a bit lost hehe)
+
+_, _, contour, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv.2CHAIN_APPROX_SIMPLE)
+for cnt in contours:
+    cv2.drawContours(roi, [cnt], -1, (0, 0, 255), 3)
+
+#Period !!

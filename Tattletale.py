cd317994fe3158ee15c00c97bec3a2539675fe53
chantiboschetti <97052239+chantiboschetti@users.noreply.github.com>
2025-04-11 16:59:36 -0400

    Update_ Introduction Tattletale.py

--- Tattletale.py
+++ Tattletale.py
@@ -13,6 +13,19 @@ Anthony Beuke
 import cv2
 import numpy as np
 
+#chanti's part - ditto on the lost hehe
+cap = cv2.VideoCapture("eye_recording.flv")
+
+while True:
+  ret, frame = cap.read()
+  roi = frame[269: 795, 537: 1416]
+  cv2.imshow("Roi", frame)
+  key = cv2.waitKey(30)
+  if key == 27:
+    break
+
+cv2.destroyAllWindows()
+
 #Contour Code (I'm a bit lost hehe)
 
 _, _, contour, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv.2CHAIN_APPROX_SIMPLE)

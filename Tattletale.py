96963740657aab7ca5f8dc8c9a2a6f63cd1733d6
onellamoitra <125746020+onella-moitra@users.noreply.github.com>
2025-04-11 16:21:35 -0400

    Create Tattletale.py

---
+++ Tattletale.py
@@ -0,0 +1,14 @@
+'''
+This is our code for the tattletale project, which trachs irises and helps determine if a user is looking away or distracted enough form their computer and sends an alert.
+
+Members:
+Onella Moitra
+Chanti Boschetti
+Kaitlyn Hull
+Rebecca Murphy
+William Rocha 
+Anthony Beuke 
+
+'''
+import cv2
+import numpy as np
